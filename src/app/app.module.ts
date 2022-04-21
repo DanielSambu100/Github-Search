88f632452585d74788a7c90fa082db08e79a845a
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-21 07:50:48 +0300

    add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GithubProfileDataComponent,
+    GithubProfileCardComponent,
+    GithubProfileComponent,
+    GithubAppComponent,
   ],
   imports: [
     BrowserModule,
