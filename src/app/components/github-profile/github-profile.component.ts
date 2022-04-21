81351491ce6b33ebc45c067522d4b1922b3fff70
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-21 15:19:58 +0300

    update2

--- src/app/components/github-profile/github-profile.component.ts
+++ src/app/components/github-profile/github-profile.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
+  details:detail[]=[
+    {id:1, name:'danielsambu100'},
+    {id:2, name:'dansam100'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
