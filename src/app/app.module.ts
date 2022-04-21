c7eff10321f8fa5c4b99f42afdef03cda89aa085
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-21 09:01:02 +0300

    update root .ts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
@@ -8,6 +8,7 @@ import { GithubProfileCardComponent } from './components/github-profile-card/git
 import { GithubProfileComponent } from './components/github-profile/github-profile.component';
 import { GithubAppComponent } from './components/github-app/github-app.component';
 import { GithubReposComponent } from './components/github-repos/github-repos.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { GithubReposComponent } from './components/github-repos/github-repos.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
